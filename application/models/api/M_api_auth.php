4caa7de444fa76cb0be29cf1f2b570d44ce63477
acep.sopian <acep.sopian@iconpln.co.id>
2018-04-06 18:41:47 +0700

    perbaikan folder api

---
+++ application/models/api/M_api_auth.php
@@ -0,0 +1,90 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class M_api_auth extends CI_Model {
+    function __construct() {
+        parent::__construct();
+    }
+    // var $client_service = "frontend-client";
+    var $auth_key       = "rshkapi";
+
+    public function check_auth_client(){
+        // $client_service = $this->input->get_request_header('Client-Service', TRUE);
+        $auth_key  = $this->input->get_request_header('Auth-Key', TRUE);
+        
+        if($auth_key == $this->auth_key){
+          // if($client_service == $this->client_service && $auth_key == $this->auth_key){
+            return true;
+        } else {
+            return json_output(401,array('status' => 401,'message' => 'Unauthorized.'));
+        }
+    }
+
+    public function login($email,$password)
+    {
+        $q  = $this->db->select('password,id_user,role')->from('m_users')->where('email',$email)->get()->row();
+
+        if($q == ""){
+             echo $email + "Email tidak ditemukan";
+            return array('status' => 401,'message' => 'Email tidak ditemukan.');
+        } else {
+            $hashed_password = $q->password;
+            $id_user              = $q->id_user;
+             $role              = $q->role;
+            // echo $hashed_password ." ".$password;
+            //echo $password;
+
+        //exit;
+             // if (hash_equals($hashed_password, crypt($password, $hashed_password))) {
+            if (hash_equals($hashed_password, md5($password))) {
+             $last_login = date('Y-m-d H:i:s');
+             $token = crypt(substr( md5(rand()), 0, 7));
+             $expired_at = date("Y-m-d H:i:s", strtotime('+12 hours'));
+             $this->db->trans_start();
+             $this->db->where('id_user',$id_user)->update('m_users',array('last_login' => $last_login));
+             $this->db->insert('m_users_authentication',array('id_user' => $id_user,'token' => $token,'expired_at' => $expired_at));
+             if ($this->db->trans_status() === FALSE){
+              $this->db->trans_rollback();
+              return array('status' => 500,'message' => 'Internal server error.');
+          } else {
+              $this->db->trans_commit();
+              return array('status' => 200,'message' => 'Successfully login.','id_user' => $id_user,'role' => $role, 'token' => $token);
+          }
+      } else {
+        return array('status' => 401,'message' => ': Passwod Salah.');
+        // echo " : Passwod Salah.";
+        exit();
+       // return array('status' => 201,'message' => 'Data has been created.');
+        return array('status' => 401,'message' => ': Passwod Salah.');
+    }
+}
+}
+
+public function logout()
+{
+    $id_user  = $this->input->get_request_header('User-ID', TRUE);
+    $token     = $this->input->get_request_header('Authorization', TRUE);
+    $this->db->where('id_user',$id_user)->where('token',$token)->delete('m_users_authentication');
+    return array('status' => 200,'message' => 'Successfully logout.');
+}
+
+public function auth()
+{
+    $id_user  = $this->input->get_request_header('User-ID', TRUE);
+    $token     = $this->input->get_request_header('Authorization', TRUE);
+    $q  = $this->db->select('expired_at')->from('m_users_authentication')->where('id_user',$id_user)->where('token',$token)->get()->row();
+    if($q == ""){
+        return json_output(401,array('status' => 401,'message' => 'Unauthorized.'));
+    } else {
+        if($q->expired_at < date('Y-m-d H:i:s')){
+            return json_output(401,array('status' => 401,'message' => 'Your session has been expired.'));
+        } else {
+            $updated_at = date('Y-m-d H:i:s');
+            $expired_at = date("Y-m-d H:i:s", strtotime('+12 hours'));
+            $this->db->where('id_user',$id_user)->where('token',$token)->update('m_users_authentication',array('expired_at' => $expired_at,'updated_at' => $updated_at));
+            return array('status' => 200,'message' => 'Authorized.');
+        }
+    }
+}
+
+}
---
+++ application/models/api/master/M_api_master_Dokter.php
@@ -0,0 +1,73 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class M_api_master_Dokter extends CI_Model {
+	function __construct() {
+		parent::__construct();
+	}
+
+
+	//public function getData($rowno,$rowperpage) {
+		public function getData($rowno,$rowperpage) {	
+		$this->db->select('*');
+		$this->db->from('m_data_dokter');
+		$this->db->limit($rowperpage, $rowno);  
+		$query = $this->db->get();
+
+		return $query->result_array();
+	}
+
+  // Select total records
+	public function getrecordCountDataDokter() {
+
+		$this->db->select('count(*) as allcount');
+		$this->db->from('m_data_dokter');
+		$query = $this->db->get();
+		$result = $query->result_array();
+
+		return $result[0]['allcount'];
+	}
+
+
+	public function getDataDokter()
+	{
+		$limit = 1;
+		$start = 0;
+
+		$this->db->select('*');
+		$this->db->from('m_data_dokter');
+		$this->db->join('m_spesialisasi', 'm_spesialisasi.id_spesialisasi = m_data_dokter.id_spesialisasi');
+		$this->db->join('m_jabatan_dokter', 'm_jabatan_dokter.id_jabatan_dokter = m_data_dokter.id_jabatan_dokter');
+		$this->db->limit($limit, $start);
+		//$this->db->record_count();
+		return $this->db->get()->result();	
+
+	}
+
+
+
+
+	public function book_detail_data($id)
+	{
+		return $this->db->select('id,title,author')->from('books')->where('id',$id)->order_by('id','desc')->get()->row();
+	}
+
+	public function book_create_data($data)
+	{
+		$this->db->insert('books',$data);
+		return array('status' => 201,'message' => 'Data has been created.');
+	}
+
+	public function book_update_data($id,$data)
+	{
+		$this->db->where('id',$id)->update('books',$data);
+		return array('status' => 200,'message' => 'Data has been updated.');
+	}
+
+	public function book_delete_data($id)
+	{
+		$this->db->where('id',$id)->delete('books');
+		return array('status' => 200,'message' => 'Data has been deleted.');
+	}
+
+}
