6a414f61192422014b3a1ec9353196d00de6adb3
mfsuada <mf.suada.mfs@gmail.com>
2018-04-13 11:16:02 +0700

    persiapan frontend

---
+++ application/views/templete/front/footer.php
@@ -0,0 +1,42 @@
+<div class="useful-links">
+	<div class="container py-5">
+		<div class="row">
+			<div class="col-12 col-md-3 justify-content-center align-self-center">
+				Call Center
+			</div>
+			<div class="col-12 col-md-6">
+				<h4>Link</h4>
+				<ul class="list-unstyled">
+					<li><a href="#">Kemenkes</a></li>
+					<li><a href="#">SIRS Online</a></li>
+					<li><a href="#">Siranap</a></li>
+					<li><a href="#">ISRUTE</a></li>
+					<li><a href="#">BPJS Kes</a></li>
+					<li><a href="#">Dinkes DKI</a></li>
+					<li><a href="#">SPGDT DKI</a></li>
+					<li><a href="#">LPSE Menkes</a></li>
+					<li><a href="#">LKPP SIRUP</a></li>
+					<li><a href="#">FK UI</a></li>
+					<li><a href="#">Yayasan Jantung Sehat</a></li>
+					<li><a href="#">PERKI</a></li>
+					<li><a href="#">PERSI</a></li>
+					<li><a href="#">BIOS PPKBLU</a></li>
+				</ul>
+			</div>
+			<div class="col-12 col-md-3 justify-content-center align-self-center">
+				<span>Kontak</span>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container py-3">
+	<div class="row">
+		<div class="col-6">
+			<span class="text-muted">&copy; Copyright PJNHK 2018. All Rights Reserved.</span>
+		</div>
+		<div class="col-6 text-right">
+			Social media links here
+		</div>
+	</div>
+</div>
